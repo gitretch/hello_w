dc0d7e35be3b280420a61b3b20c006f3077f0b8c
Hilary Sasso-Schleh <hsassoschleh@gmail.com>
2020-07-16 11:32:16 -0400

    initial commit with about contact and index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import Header from "../components/header"
+import {Link} from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <Link to="/contact/">Contact Info</Link>
+      <Header headerText="home"/>
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  );
 }
